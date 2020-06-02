dc4a587b5135722672e695792adb986a0f4e1be7
Igor Halfeld <hello@igorluiz.me>
2020-06-02 14:59:35 -0300

    initial commit :tada:

---
+++ README.md
@@ -0,0 +1,33 @@
+## Vue Hotkey Estrategico
+
+> Estratégia na hora de registrar um evento do teclado
+
+### Instalar
+
+```
+npm install git+ssh//
+```
+
+### Setup
+
+```js
+import Vue from 'vue'
+import VueHotkeyEstrategico from 'vue-hotkey-estrategico'
+
+Vue.use(VueHotkeyEstrategico, {
+  prevent: ['input', 'textarea'] // default configs
+})
+```
+
+### Uso
+
+```js
+
+// Register a binding
+this.$VueHotkey.register('ctrl+a', (event) => {
+  // handle event
+})
+
+// Unregister a binding
+this.$VueHotkey.unregister('ctrl+a')
+```
---
+++ index.js
@@ -0,0 +1,53 @@
+import hotkeys from 'hotkeys-js'
+
+function defineReadonlyProperty (object, property, value) {
+  Object.defineProperty(object, property, {
+    get () {
+      return value
+    },
+    set () {
+      throw new Error(`You can't set ${property}`)
+    }
+  })
+}
+
+function install (Vue, opts) {
+  const defaultOptions = {
+    prevent: ['input', 'textarea']
+  }
+  const options = { ...defaultOptions, ...opts }
+
+  const vueHotKey = {
+    register (key, fn, hotkeysOpts) {
+      if (!key) {
+        throw new Error('You need to pass a key')
+      }
+      if (!fn) {
+        throw new Error('You need to pass a callback')
+      }
+
+      const keybindingConfigs = {
+        keyup: true,
+        keydown: false,
+        keypress: false
+      }
+
+      hotkeys(key, { ...keybindingConfigs, ...hotkeysOpts }, (event, handler) => {
+        const element = event.target || event.srcElement
+        if (options.prevent.includes(element)) {
+          return
+        }
+        fn(event, handler)
+      })
+    },
+    unregister (key) {
+      hotkeys.unbind(key)
+    }
+  }
+
+  defineReadonlyProperty(Vue.prototype, '$VueHotkey', vueHotKey)
+}
+
+export default {
+  install
+}
